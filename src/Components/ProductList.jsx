c6a6392adc9f5a8229f675b0cdf5a2f6b5100608
Yuval Shpitzer <shpitzery10@gmail.com>
2024-10-17 17:31:46 +0300

    added a map func to render a list of the products

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -13,6 +13,14 @@ const ProductList = () => {
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
+        {products.map(product => ( //products.map(...) is a JavaScript expression, and in JSX, we can’t write JavaScript code directly without using {}. The curly braces tell React to interpret the contents as JavaScript rather than plain text or HTML.
+          <li key={product.id} className='product-list-item'> {/* The key prop allows React to efficiently update and re-render only the items that have changed */}
+          <span>{product.name} - ${product.price}</span> {/* The <span> tag is used simply to wrap the product information for styling and structure purposes */}
+          <button>
+            Add to Cart
+          </button>
+          </li>
+        ))}
      
       </ul>
     </div>
